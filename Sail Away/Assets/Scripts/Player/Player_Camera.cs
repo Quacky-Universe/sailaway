9af624ae43a3fd70ffc3fe051aff093b31ac14d5
Sonicboomcolt <littiled3131@gmail.com>
2020-03-18 14:21:02 -0400

    Created movement prototype
    This is for showing the movement of the player.

---
+++ Sail Away/Assets/Scripts/Player/Player_Camera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Camera : MonoBehaviour
+{
+    private Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    private void Start()
+    {
+        //Find the player transform
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+    }
+}
---
+++ Sail Away/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Player Movement Stats")]
+    [SerializeField] private float Speed;
+    [SerializeField] private float turnSpeed = 15f;
+
+    //Player inputs.
+    private float InputX;
+    private float InputY;
+
+    public Rigidbody playerRB;
+
+    private void FixedUpdate()
+    {
+        //Detect player input 
+        InputX = Input.GetAxis("Horizontal");
+        InputY = Input.GetAxis("Vertical");
+
+        //Take input and clamp it so the player can't go faster by pressing both diagonal keys at once.
+        Vector3 MoveDir = new Vector3(InputX, 0.0f, InputY);
+        MoveDir = Vector3.ClampMagnitude(MoveDir, 1);
+
+
+        //Rotate the player based on the moveDir
+        if (MoveDir.magnitude > .1f)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(MoveDir), Time.deltaTime * turnSpeed);
+        }
+        else
+        {
+            playerRB.velocity = Vector3.zero;
+        }
+
+        //Move the player
+        playerRB.MovePosition(Vector3.Lerp(transform.position, transform.position + MoveDir, Speed * Time.deltaTime));
+    }
+}
