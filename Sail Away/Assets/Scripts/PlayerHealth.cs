fce62ddabff87fb7f5ecb97feaa95f606d1d5faa
CristiHKJ <cr60hkj27@gmail.com>
2020-04-29 13:52:43 +0300

    Damage / Health System (needs polishing like effects and screen shake)

---
+++ Sail Away/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : CharacterHealth
+{
+    public override void Die()
+    {
+        base.Die();
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
--- Sail Away/Assets/Scripts/Weapons/Weapons_Projectile.cs
+++ Sail Away/Assets/Scripts/Weapons/Weapons_Projectile.cs
@@ -2,9 +2,21 @@
 
 public class Weapons_Projectile : MonoBehaviour
 {
+    public static float damage = 10f;
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.CompareTag("Island"))
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            if (collision.transform.GetComponent<CharacterHealth>() != null)
+            {
+                CharacterHealth characterHealth = collision.transform.GetComponent<CharacterHealth>();
+                characterHealth.TakeDamage(damage);
+
+                Destroy(gameObject);
+            }
+        }
+        else if (collision.transform.CompareTag("Island"))
         {
             Destroy(gameObject);
         }
