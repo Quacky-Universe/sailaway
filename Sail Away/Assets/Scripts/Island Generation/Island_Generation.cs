aed60c1b09ce0a5b4d95b5d0ceb22d45c02a3416
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-07 18:56:51 +0530

    Added island gen (a bad version) and built it

---
+++ Sail Away/Assets/Scripts/Island Generation/Island_Generation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island_Generation : MonoBehaviour
+{
+
+    public GameObject island;
+    public float xspread = 1000;
+    public float yspread = 0;
+    public float zspread = 1000;
+    public int num = 100;
+    public Quaternion rotation = new Quaternion(0f,0f,0f, 0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < num;  i++)
+        {
+            SpreadItem();
+
+        }
+    }
+
+    void SpreadItem(){
+        Vector3 randposition = new Vector3(Random.Range(-xspread,xspread),Random.Range(-yspread,yspread),Random.Range(-zspread,zspread)) + transform.position;
+        GameObject clone = Instantiate(island, randposition,island.transform.localRotation );
+        //clone.transform.position.y = island.transform.position.y;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
