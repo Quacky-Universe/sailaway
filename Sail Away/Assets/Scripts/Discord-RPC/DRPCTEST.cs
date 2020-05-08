f51056dfa30e0b7327284f740aff87d1a85d623f
TheSilentDefender <madhavkapa16@gmail.com>
2020-05-08 15:05:36 +0530

    ADDED A BETTER GENERATION AND ALSO DISCORD RPC

---
+++ Sail Away/Assets/Scripts/Discord-RPC/DRPCTEST.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DiscordPresence;
+
+public class DRPCTEST : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PresenceManager.UpdatePresence(detail: "yaya");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
