5f69262af20eaf719cf1b5055b8147bedf4b53c7
Madhav Kapa <madhavkapa16@gmail.com>
2020-05-19 20:54:20 +0530

    :)

---
+++ Sail Away/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject enemy;
+
+    public Transform player;
+
+    #region Singleton
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+}
