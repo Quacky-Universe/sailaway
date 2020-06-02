420b8db77e0ebb3d14f4324ab7d10d9a4d262495
Madhav Kapa <madhavkapa16@gmail.com>
2020-06-02 07:54:10 +0530

    :)

---
+++ Sail Away/Assets/Scripts/Main Menu/OptionsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class OptionsMenu : MonoBehaviour
+{ 
+    public GameObject player;
+
+    // Start is called before the first frame update
+    public AudioMixer MainVolume;
+    public void SetVolume(float volume)
+    {
+        MainVolume.SetFloat("MainVolume", volume);
+    }
+
+    public void SetGraphics(int QualityIndex)
+    {
+        QualitySettings.SetQualityLevel(QualityIndex);
+    }
+    
+}
