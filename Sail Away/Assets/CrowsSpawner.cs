f4852855724ab334553b2274a553329f7a57426e
CristiHKJ <cr60hkj27@gmail.com>
2020-08-10 14:08:48 +0300

    Crows Spawner added (crows still missing)

---
+++ Sail Away/Assets/CrowsSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CrowsSpawner : MonoBehaviour
+{
+    float afkTime = 0f;
+    public float crowStartTime = 30f;
+
+    bool crowHasSpawned = false;
+
+    [SerializeField] LayerMask layerMask;
+
+    void Update()
+    {
+        if (!Input.anyKeyDown)
+        {
+            afkTime += Time.deltaTime;
+
+            if (afkTime >= crowStartTime && !crowHasSpawned)
+            {
+                StartCoroutine(SpawnCrow());
+            }
+        }
+        else
+        {
+            if (afkTime >= 0f)
+            {
+                afkTime = 0f;
+                StopAllCoroutines();
+            }
+        }
+    }
+
+    IEnumerator SpawnCrow()
+    {
+        crowHasSpawned = false;
+        transform.localPosition = new Vector3(Random.Range(-5f, 5f), 10f, Random.Range(-5f, 5f));
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, layerMask))
+        {
+            //TODO: Spawn crow
+            Debug.Log("Crow spawned");
+            crowHasSpawned = true;
+            yield return new WaitForSeconds(Random.Range(10f, 16f));
+            yield return StartCoroutine(SpawnCrow());
+        }
+        else
+        {
+            yield return StartCoroutine(SpawnCrow());
+        }
+    }
+}
